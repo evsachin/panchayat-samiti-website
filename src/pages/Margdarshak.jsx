0e91efc99479b1318aa3a5ef48080844f5657103
wtsachin <sachin.kolhe.evisiontech@gmail.com>
2025-04-23 19:51:42 +0530

    added

--- src/pages/Margdarshak.jsx
+++ src/pages/Margdarshak.jsx
@@ -39,7 +39,7 @@ export default function Margdarshak() {
         {officers.map((officer, index) => (
           <motion.div
             key={index}
-            className="bg-white shadow-2xl hover:shadow-blue-300  overflow-hidden transition-all duration-300 hover:scale-105 lg:px-8"
+            className="bg-white shadow-2xl hover:shadow-blue-300  overflow-hidden transition-all duration-300 hover:scale-105 lg:mx-8 p-4 rounded-2xl"
             initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true, amount: 0.3 }}
@@ -48,7 +48,7 @@ export default function Margdarshak() {
             <img
               src={officer.image}
               alt={officer.name}
-              className="w-full h-115 object-fill border-4 rounded-2xl p-4"
+              className="w-full h-115 object-fill border-4 rounded-2xl"
             />
             <div className="p-4 text-center">
               <h2 className="text-xl font-semibold text-gray-800 mb-2">
