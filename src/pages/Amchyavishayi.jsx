302d73576df654e7f2a7aedd0266803557450653
wtsachin <sachin.kolhe.evisiontech@gmail.com>
2025-05-06 09:42:32 +0530

    added link in amachyavishayi

--- src/pages/Amchyavishayi.jsx
+++ src/pages/Amchyavishayi.jsx
@@ -13,23 +13,22 @@ function Amachyavishayi() {
           हा आमचा उद्देश आहे.
         </p>
       </div>
-      <div className="bg-gray-100 min-h-screen p-2">
-        <div className="bg-white md:p-6 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold mb-4 text-blue-800 p-4">
-            शिक्षण विभाग पंचायत समिती पारनेर
-          </h2>
+      
 
-          <div className="w-full h-[90vh]">
-            <iframe
-              src="
-https://drive.google.com/file/d/1uA1Jyb7c2V9wMtBdxpLZWFQ9fTIYV_Yx/preview"
-              width="100%"
-              height="100%"
-              allow="autoplay"
-              className="rounded-lg border-2"
-              title="Z P Schools PDF"
-            ></iframe>
-          </div>
+      <div className="bg-gray-100 min-h-screen p-4">
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <ul className="list-disc pl-6 text-blue-700">
+            <li>
+              <a
+                href="https://drive.google.com/file/d/1uA1Jyb7c2V9wMtBdxpLZWFQ9fTIYV_Yx/view"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xl font-semibold hover:underline"
+              >
+                नवोदय विद्यालय प्रवेश परीक्षा यादी
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
